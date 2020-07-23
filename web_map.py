95a90ed02736ae9eb76af2fb90f52bedc25aaa79
DamianDuy <gamergray8@gmail.com>
2020-07-23 21:46:04 +0200

    Edited markers' style

--- web_map.py
+++ web_map.py
@@ -12,15 +12,29 @@ latitUnvis = list(dataUnvis["LAT"])
 longiUnvis = list(dataUnvis["LON"])
 nameUnvis = list(dataUnvis["NAME"])
 
+def colouring(rate):
+    extrRate = rate[0] + rate[1]
+    if extrRate == "10":
+        return "blue"
+    else:
+        return "green"    
+
+def iconing(rate):
+    extrRate = rate[0] + rate[1]
+    if extrRate == "10":
+        return "heart-o"
+    else:
+        return "check-circle-o" 
+
 
 map = folium.Map(location = [52.06, 19.29], zoom_start = 6, tiles = "Stamen Terrain")
 fgVis = folium.FeatureGroup(name = "Visited Markers")
 fgUnvis = folium.FeatureGroup(name = "Unvisited Markers")
 
 for lat, lon, nam, rat in zip(latitVis, longiVis, nameVis, rates):
-    fgVis.add_child(folium.Marker(location = [lat, lon], popup = str(nam) + "\n" + str(rat), icon = folium.Icon(color = 'green')))
+    fgVis.add_child(folium.Marker(location = [lat, lon], popup = folium.Popup(str(nam) + "\n" + str(rat), parse_html = True), icon = folium.Icon(color = colouring(rat), icon = iconing(rat), prefix = "fa")))
 for lat, lon, nam in zip(latitUnvis, longiUnvis, nameUnvis):
-    fgUnvis.add_child(folium.Marker(location = [lat, lon], popup = str(nam), icon = folium.Icon(color = 'red')))
+    fgUnvis.add_child(folium.Marker(location = [lat, lon], popup = folium.Popup(str(nam), parse_html = True), icon = folium.Icon(color = "red", icon = "clock-o", prefix = "fa")))
 
 map.add_child(fgVis)
 map.add_child(fgUnvis)
