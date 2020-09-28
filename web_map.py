5919b89e145617737739d2db9b0190462ffeab5d
DamianDuy <gamergray8@gmail.com>
2020-09-28 11:54:01 +0200

    Rewrote web_map.py in OOP

--- web_map.py
+++ web_map.py
@@ -1,49 +1,64 @@
 import folium
 import pandas
 
-dataVis = pandas.read_csv("visited.txt")
-latitVis = list(dataVis["LAT"])
-longiVis = list(dataVis["LON"])
-nameVis = list(dataVis["NAME"])
-rates = list(dataVis["RATE"])
+class Webmap:
+    def __init__(self):
+       self.dataVis = pandas.read_csv("visited.txt")
+       self.latitVis = list(self.dataVis["LAT"])
+       self.longiVis = list(self.dataVis["LON"])
+       self.nameVis = list(self.dataVis["NAME"])
+       self.rates = list(self.dataVis["RATE"])
 
-dataUnvis = pandas.read_csv("to_visit.txt")
-latitUnvis = list(dataUnvis["LAT"])
-longiUnvis = list(dataUnvis["LON"])
-nameUnvis = list(dataUnvis["NAME"])
+       self.dataUnvis = pandas.read_csv("to_visit.txt")
+       self.latitUnvis = list(self.dataUnvis["LAT"])
+       self.longiUnvis = list(self.dataUnvis["LON"])
+       self.nameUnvis = list(self.dataUnvis["NAME"])
 
-min_popul = 15000000
-mid_popul = 25000000
+       self.min_popul = 15000000
+       self.mid_popul = 25000000
 
-def colouring(rate):
-    extrRate = rate[0] + rate[1]
-    if extrRate == "10":
-        return "blue"
-    else:
-        return "green"    
+       self.map = folium.Map(location = [52.06, 19.29], zoom_start = 6, tiles = "Stamen Terrain")
+       self.fgVis = folium.FeatureGroup(name = "Visited Markers")
+       self.fgUnvis = folium.FeatureGroup(name = "Unvisited Markers")
+       self.fgPopul = folium.FeatureGroup(name = "Population Layer")
+    
+    @staticmethod
+    def colouring(rate):
+       extrRate = rate[0] + rate[1]
+       if extrRate == "10":
+           return "blue"
+       else:
+           return "green"    
 
-def iconing(rate):
-    extrRate = rate[0] + rate[1]
-    if extrRate == "10":
-        return "heart-o"
-    else:
-        return "check-circle-o" 
+    @staticmethod
+    def iconing(rate):
+       extrRate = rate[0] + rate[1]
+       if extrRate == "10":
+           return "heart-o"
+       else:
+           return "check-circle-o"
+    
+    def creatingMarkers(self):
+       for lat, lon, nam, rat in zip(self.latitVis, self.longiVis, self.nameVis, self.rates):
+           self.fgVis.add_child(folium.Marker(location = [lat, lon], popup = folium.Popup(str(nam) + "\n" + str(rat), parse_html = True), icon = folium.Icon(color = Webmap.colouring(rat), icon = Webmap.iconing(rat), prefix = "fa")))
+       for lat, lon, nam in zip(self.latitUnvis, self.longiUnvis, self.nameUnvis):
+           self.fgUnvis.add_child(folium.Marker(location = [lat, lon], popup = folium.Popup(str(nam), parse_html = True), icon = folium.Icon(color = "red", icon = "clock-o", prefix = "fa"))) 
+    
+    def creatingPopulLayer(self):
+        self.fgPopul.add_child(folium.GeoJson(data = open("world.json", "r", encoding = "utf-8-sig").read(), style_function = lambda x: {"fillColor" : "green" if x["properties"]["POP2005"] < self.min_popul else "yellow" if self.min_popul <= x["properties"]["POP2005"] < self.mid_popul else "red"}))
 
+    def mapAdd(self):
+        self.map.add_child(self.fgVis)
+        self.map.add_child(self.fgUnvis)
+        self.map.add_child(self.fgPopul)
+        self.map.add_child(folium.LayerControl())
+        self.map.save("map.html")
 
-map = folium.Map(location = [52.06, 19.29], zoom_start = 6, tiles = "Stamen Terrain")
-fgVis = folium.FeatureGroup(name = "Visited Markers")
-fgUnvis = folium.FeatureGroup(name = "Unvisited Markers")
-fgPopul = folium.FeatureGroup(name = "Population Layer")
+def main():
+    visMap = Webmap()
+    visMap.creatingMarkers()
+    visMap.creatingPopulLayer()
+    visMap.mapAdd()
 
-for lat, lon, nam, rat in zip(latitVis, longiVis, nameVis, rates):
-    fgVis.add_child(folium.Marker(location = [lat, lon], popup = folium.Popup(str(nam) + "\n" + str(rat), parse_html = True), icon = folium.Icon(color = colouring(rat), icon = iconing(rat), prefix = "fa")))
-for lat, lon, nam in zip(latitUnvis, longiUnvis, nameUnvis):
-    fgUnvis.add_child(folium.Marker(location = [lat, lon], popup = folium.Popup(str(nam), parse_html = True), icon = folium.Icon(color = "red", icon = "clock-o", prefix = "fa")))
-
-fgPopul.add_child(folium.GeoJson(data = open("world.json", "r", encoding = "utf-8-sig").read(), style_function = lambda x: {"fillColor" : "green" if x["properties"]["POP2005"] < min_popul else "yellow" if min_popul <= x["properties"]["POP2005"] < mid_popul else "red"}))
-
-map.add_child(fgVis)
-map.add_child(fgUnvis)
-map.add_child(fgPopul)
-map.add_child(folium.LayerControl())
-map.save("map.html")
+if __name__ == "__main__":
+    main()
